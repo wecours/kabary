778d3dd5129bf17232546518aecdc0dbfc5bceca
Mahefa <abelmahefa@gmail.com>
2023-10-24 13:49:59 +0300

    Add header

--- src/Ohabolana/Ohabolana.tsx
+++ src/Ohabolana/Ohabolana.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Ohabolana.css';
 import AutocompleteSearch from './AutocompleteSearch';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 function Ohabolana() {
   return (
     <div className="Ohabolana">
       <header className="App-header">
-        ohabolana
+        
+      <Box sx={{ width: '100%', maxWidth: 500 }}>
+        <Typography variant="h1" gutterBottom>
+          Ohabolana
+        </Typography>
+      </Box>
 
         <AutocompleteSearch
         />
