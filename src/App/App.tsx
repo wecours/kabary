27ed9e61db97631246a8329b7a9570708131f1d3
Mahefa <abelmahefa@gmail.com>
2023-10-24 12:26:01 +0300

    App Drawer

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import MediaCard from '../Components/MediaCard';
 
 export default function App() {
   return (
-    <React.Fragment key={"left"}>
+    <React.Fragment key={"1"}>
 
       <MediaCard />
 
-    <Button onClick={toggleDrawer("left", true)}>Left</Button>
-    <Drawer
-      anchor={"left"}
-      open={state["left"]}
-      onClose={toggleDrawer("left", false)}
-    >
-      {/* {list(anchor)} */}
-    </Drawer>
-  </React.Fragment>
+    </React.Fragment>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import AppDrawer from './App/AppDrawer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppDrawer />
   </React.StrictMode>
 );
 
